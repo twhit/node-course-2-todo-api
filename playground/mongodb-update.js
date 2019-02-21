1c2e58cef7d9132739d90d2168a28decca60cc20
twhit <whitfield.tim.53@gmail.com>
2019-02-21 17:57:19 -0500

    Add Update Script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // findOneAndUpdate
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5c6f233648c7650d52a6b5b0')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5c6f186d60a23926659386be')
+  }, {
+    $set: {
+      name: 'Tim'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+  returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //db.close();
+});
