d9c20fb595cd92788a6409a48bec779827c72865
twhit <whitfield.tim.53@gmail.com>
2019-02-21 22:35:18 -0500

    Add DELETE todo by ID

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-  var id = req.params.id
+  var id = req.params.id;
   if (!ObjectID.isValid(id)) {
     console.log('ID not valid');
     return res.status(404).send();
@@ -44,6 +44,21 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
     res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
   }).catch((e) => {
     res.status(400).send();
   });
